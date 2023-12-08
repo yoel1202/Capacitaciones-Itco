a478aa255563b802de0dc9cbd8648839084aee4c
yoel cerdas <ycerdas@itcoint.com>
2023-12-08 13:27:10 -0600

    first commit

---
+++ webBlazor/webBlazor/Models/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace webBlazor.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CardId { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+    }
+}
